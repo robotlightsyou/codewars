e27393486ef465cd54f28908e8719f0df099aca9
Adam Earle <robotlightsyou@gmail.com>
2020-09-13 13:47:59 -0700

    remove remotes

--- basics/duty free.py
+++
@@ -1,8 +0,0 @@
-def duty_free(price, discount, holiday_cost):
-    savings = ((price * 100) * discount) / 10000
-    print(savings)
-    return int(holiday_cost // savings)
-
-
-print(duty_free(12, 50, 1000))  # , 166)
-print(duty_free(17, 10, 500))  # , 294)
