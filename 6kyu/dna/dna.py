a83bdd2de98853e5602f9e0f9de744f80f9c2a3d
Adam Earle <robotlightsyou@gmail.com>
2020-09-25 16:15:59 -0700

    dna passed
    switched dna to regex method and it was insanely simpler. followed advice at
    
    https://stackoverflow.com/questions/1155376/python-re-find-longest-sequence

--- 6kyu/dna/dna.py
+++ 6kyu/dna/dna.py
@@ -1,6 +1,7 @@
 from sys import argv, exit
 import csv
 from pprint import pprint
+import re
 
 
 def main():
@@ -10,16 +11,20 @@ def main():
     # dna_string = get_dna_string(dnafile=argv[2]).rstrip()
     # dna_dict, people_list = get_people(peoplefile=argv[1])
     dna_dict, people_list = get_people(peoplefile='./databases/large.csv')
-    dna_string = get_dna_string(dnafile='./sequences/5.txt').rstrip()
+    dna_string = get_dna_string(dnafile='./sequences/7.txt').rstrip()
     totals = calc_STR(dna_string=dna_string, str_list=dna_dict['SRTs'])
     string_numbers = make_strings(totals)
     print(dna_match(people=people_list, numbers=string_numbers))
 
     # print(dna_match(people=people_list, numbers=totals))
     # print(dna_string.rstrip())
-    pprint(dna_dict)
-    pprint(people_list['Lavender'])
-    print(string_numbers)
+    # pprint(dna_dict)
+    # pprint(people_list['Lavender'])
+    # print(string_numbers)
+    # if string_numbers == people_list['Lavender']:
+    #     print("equal")
+    # else:
+    #     print("not")
 
 
 def check_args(argument=[]):
@@ -52,27 +57,35 @@ def get_dna_string(dnafile=''):
         return dna_string
 
 
-def calc_STR(dna_string='', str_list=[]):
-    ''' compute longest run of eact STR in DNA '''
+def calc_STR(str_list=[], dna_string=''):
     count_total = []
-    # print(str_list)
-    # print(count)
-    # for index, STR in enumerate(str_list):
     for STR in str_list:
-        length = len(STR)
-        count = 0
-        max_count = 0
-        for i, v in enumerate(dna_string):
-            if v == STR[0]:
-                count += is_match(STR=STR,
-                                  dna_string=dna_string[i:i+length])
-            sample = dna_string[i-(2 * length):i+(2*length)]
-            if count > 1 and not is_repeat(dna=dna_string[i-length:i], match=STR, sample=sample):
-                if count > max_count:
-                    max_count = count
-                count = 0
-        count_total.append(count)
+        matches = re.findall(rf'((?:{STR})+)', dna_string)
+        longest_match = max(matches, default='')
+        count_total.append(longest_match.count(STR))
     return count_total
+
+# def calc_STR(dna_string='', str_list=[]):
+#     ''' compute longest run of eact STR in DNA '''
+#     count_total = []
+#     # print(str_list)
+#     # print(count)
+#     # for index, STR in enumerate(str_list):
+#     for STR in str_list:
+#         length = len(STR)
+#         count = 0
+#         max_count = 0
+#         for i, v in enumerate(dna_string):
+#             if v == STR[0]:
+#                 count += is_match(STR=STR,
+#                                   dna_string=dna_string[i:i+length])
+#             sample = dna_string[i-(2 * length):i+(2*length)]
+#             if count > 1 and not is_repeat(dna=dna_string[i-length:i], match=STR, sample=sample):
+#                 if count > max_count:
+#                     max_count = count
+#                 count = 0
+#         count_total.append(count)
+#     return count_total
 
 
 def is_match(STR='', dna_string=''):
@@ -97,16 +110,17 @@ def make_strings(number_list=[]):
 def dna_match(people={}, numbers=[]):
     # print(people)
 
-    for person in people:
-        ''' 
+    for person in people.keys():
+        '''
         if count(STR) == person from CSV -> print person.name,
-        if no match print 'No match' 
+        if no match print 'No match'
         '''
         # print(f"person is {person}\nnumbers is {numbers}")
         # print(f"person's srts are {people[person]}\n")
-        if people[person] == numbers:
+        test = people[person]
+        if test == numbers:
             return person
-        return 'No match'
+    return 'No match'
 
 
 if __name__ == '__main__':
