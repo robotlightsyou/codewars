1cc198ddd32542a400ffaccb09545e9530237ff7
Adam Earle <robotlightsyou@gmail.com>
2020-09-25 00:55:12 -0700

    fully functioning?

--- 6kyu/dna/dna.py
+++ 6kyu/dna/dna.py
@@ -4,24 +4,27 @@ import csv
 
 def main():
     check_args(argument=argv)
-    dna_list, people_list = get_people(peoplefile=argv[1])
+    dna_dict, people_list = get_people(peoplefile=argv[1])
     dna_string = get_dna_string(dnafile=argv[2]).rstrip()
-    totals = calc_STR(dna_string=dna_string, str_list=people_list)
-    print(dna_match(people=people_list, numbers=totals))
-    print(dna_string.rstrip())
-    print(dna_list)
-    print(people_list)
+    totals = calc_STR(dna_string=dna_string, str_list=dna_dict['SRTs'])
+    string_numbers = make_strings(totals)
+    print(dna_match(people=people_list, numbers=string_numbers))
+
+    # print(dna_match(people=people_list, numbers=totals))
+    # print(dna_string.rstrip())
+    # print(dna_list)
+    # print(people_list)
 
 
 def check_args(argument=[]):
-    ''' incorrect len(aegv) -> print error '''
+    ''' incorrect len(argv) -> print error '''
     if len(argument) != 3:
-        print('Please include the correct files')
+        print('Please include the correct number of files')
         exit(1)
 
 
 def get_people(peoplefile=''):
-    ''' first CLA -> CSV file open CSV and read contents into memory first row 
+    ''' first CLA -> CSV file open CSV and read contents into memory first row
     will be column names starting with 'name' and followed by str sequences '''
     with open(peoplefile, newline='') as f:
         fieldnames = ['name']
@@ -31,7 +34,7 @@ def get_people(peoplefile=''):
             if index == 0:
                 dna_list = row
             else:
-                print(row['name'], row['SRTs'])
+                # print(row['name'], row['SRTs'])
                 CSV[row['name']] = row['SRTs']
         return dna_list, CSV
 
@@ -47,22 +50,32 @@ def calc_STR(dna_string='', str_list=[]):
     ''' compute longest run of eact STR in DNA '''
     count = [0, 0, 0]
     for index, STR in enumerate(str_list):
-        for i in dna_string:
-            if i == STR[0]:
-                count[index] += is_match(STR=STR, dna_string=dna_string)
+        length = len(STR)
+        for i, v in enumerate(dna_string):
+            if v == STR[0]:
+                count[index] += is_match(STR=STR,
+                                         dna_string=dna_string[i:i+length])
     return count
 
 
 def is_match(STR='', dna_string=''):
     ''' check if seq matces STR, return 1 if true else 0 '''
-    pass
+    return 1 if STR == dna_string else 0
+
+
+def make_strings(number_list=[]):
+    return [str(i) for i in number_list]
 
 
 def dna_match(people={}, numbers=[]):
+    # print(people)
+
     for person in people:
-        if person['SRTs'] == numbers:
-            return person['name']
-        return 'No match'
+        # print(f"person is {person}\nnumbers is {numbers}")
+        # print(f"person's srts are {people[person]}\n")
+        if people[person] == numbers:
+            return person
+    return 'No match'
 
 
 # if count(STR) == person from CSV -> print person.name
