71cbc154e956e7a0856c0c86b7c0b5006fe886d7
Adam Earle <robotlightsyou@gmail.com>
2020-09-28 22:48:27 -0700

    week 7 lecture

---
+++ 6kyu/seven/imdb/search.py
@@ -0,0 +1,10 @@
+import csv
+
+title = input("title: ")
+
+with open("shows2.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        if title == row["primaryTitle"]:
+            print(row["primaryTitle"], row["startYear"])
