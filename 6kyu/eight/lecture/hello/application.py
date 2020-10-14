2d2a4a551994f5758d4bb42c468b70f4d7f31583
Adam Earle <robotlightsyou@gmail.com>
2020-10-13 18:25:09 -0700

    hello complete

--- 6kyu/eight/lecture/hello/application.py
+++ 6kyu/eight/lecture/hello/application.py
@@ -4,11 +4,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html", name="Emma")
 
+
 @app.route("/hello")
 def hello():
     name = request.args.get("name")
+    if not name:
+        return render_template("failure.html")
     return render_template("hello.html", name=name)
